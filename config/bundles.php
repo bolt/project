e061af73b41076cb0b007c4c942e06c6bde27071
Bob den Otter <bob@twokings.nl>
2019-02-01 16:37:30 +0100

    first commit

---
+++ config/bundles.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
+];
